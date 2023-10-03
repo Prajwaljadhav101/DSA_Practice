95d0b3f26c11247fa6b9488b5e334d6529560e81
Prajwaljadhav101 <prajwaljadhav1035@gmail.com>
2023-10-03 18:06:41 +0530

    lecture 16

---
+++ lecture16/selectionsort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+// #include<algorithm>
+using namespace std;
+
+void selectionSort(vector<int>& v){
+
+    int n=v.size();
+    for(int i=0; i<n-1; i++){
+        int minIndex=i;
+
+        for(int j=i+1; j<n; j++){
+            if(v[j] < v[minIndex]){
+                minIndex = j;
+            }
+        }
+        swap(v[i],v[minIndex]);
+    }
+}
+
+int main(){
+    
+    // vector<int> v={64,25,12,22,11};
+    vector<int> v={6,2,8,4,10};
+    selectionSort(v);
+    for(int i=0; i<v.size(); i++){
+        cout<<v[i]<<" ";
+    }
+    return 0;
+    
+}

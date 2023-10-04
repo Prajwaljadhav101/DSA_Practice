64f8b80c699524e930e7c1580af3bcd45eac3fbf
Prajwaljadhav101 <prajwaljadhav1035@gmail.com>
2023-10-04 16:57:05 +0530

    file changes

--- lecture17/bubblesort.cpp
+++ lecture17/bubblesort.cpp
@@ -6,11 +6,16 @@ void bubbleSort(vector<int>& v){
 
     int n = v.size();
     for(int i=0; i<n-1; i++){
+        bool flag = false;
 
         for(int j=0; j<n-i-1; j++){
             if(v[j]>v[j+1]){
                 swap(v[j],v[j+1]);
+                flag = true;
             }
+        }
+        if(flag == false){      //if nothing is swapped then break the loop as array is already sorted.
+            break;
         }
     }
 
